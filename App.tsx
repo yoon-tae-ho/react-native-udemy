def8a169338ba91bf401cf384a757c035c820085
yoontaeho <pln0302@yonsei.ac.kr>
2023-01-23 14:33:51 +0900

    Initialized Section 12.

--- App.tsx
+++ App.tsx
@@ -1,57 +1,13 @@
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { StatusBar } from "expo-status-bar";
+import { Text, View } from "react-native";
 
-import LoginScreen from "./src/screens/LoginScreen";
-import SignupScreen from "./src/screens/SignupScreen";
-import WelcomeScreen from "./src/screens/WelcomeScreen";
-import { Colors } from "./constants/styles";
-
-const Stack = createNativeStackNavigator();
-
-function AuthStack() {
+const App = () => {
   return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: { backgroundColor: Colors.primary500 },
-        headerTintColor: "white",
-        contentStyle: { backgroundColor: Colors.primary100 },
-      }}
-    >
-      <Stack.Screen name="Login" component={LoginScreen} />
-      <Stack.Screen name="Signup" component={SignupScreen} />
-    </Stack.Navigator>
-  );
-}
-
-function AuthenticatedStack() {
-  return (
-    <Stack.Navigator
-      screenOptions={{
-        headerStyle: { backgroundColor: Colors.primary500 },
-        headerTintColor: "white",
-        contentStyle: { backgroundColor: Colors.primary100 },
-      }}
-    >
-      <Stack.Screen name="Welcome" component={WelcomeScreen} />
-    </Stack.Navigator>
-  );
-}
-
-function Navigation() {
-  return (
-    <NavigationContainer>
-      <AuthStack />
-    </NavigationContainer>
-  );
-}
-
-export default function App() {
-  return (
-    <>
+    <View>
       <StatusBar style="light" />
-
-      <Navigation />
-    </>
+      <Text>Hello World!</Text>
+    </View>
   );
-}
+};
+
+export default App;
